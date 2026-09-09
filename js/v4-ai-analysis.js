253779734a2d0a859b70db5d832bd758b253054e
takutomo-momo <takutomo.momo2015@gmail.com>
2026-09-09 21:25:05 +0900

    AI解析ページの通信不具合を修正

--- js/v4-ai-analysis.js
+++ js/v4-ai-analysis.js
@@ -573,11 +573,6 @@
                     method:
                         "POST",
 
-                    headers: {
-                        "Content-Type":
-                            "application/json"
-                    },
-
                     body:
                         JSON.stringify({
                             action:
